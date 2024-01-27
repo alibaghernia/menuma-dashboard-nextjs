0e7c6ef914a910e989c546a5f10cbd39f67f6e58
Abolfazl Akbarzadeh <abolfazlakbarzadeh80@gmail.com>
2024-01-27 11:07:54 +0330

    - cleanup - install ant design

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,8 +1,43 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-import "./globals.css";
-
-const inter = Inter({ subsets: ["latin"] });
+import LocalFont from "next/font/local";
+import { Vazirmatn } from "next/font/google";
+import "./globals.scss";
+import { AntdRegistry } from "@ant-design/nextjs-registry";
+import { ConfigProvider } from "antd";
+const vazirMatn = LocalFont({
+  src: [
+    {
+      path: "../assets/fonts/vazirFD-100.woff2",
+      weight: "100",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/vazirFD-300.woff2",
+      weight: "300",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/vazirFD-400.woff2",
+      weight: "400",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/vazirFD-500.woff2",
+      weight: "500",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/vazirFD-700.woff2",
+      weight: "700",
+      style: "normal",
+    },
+    {
+      path: "../assets/fonts/vazirFD-900.woff2",
+      weight: "900",
+      style: "normal",
+    },
+  ],
+});
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -15,8 +50,26 @@ export default function RootLayout({
   children: React.ReactNode;
 }>) {
   return (
-    <html lang="en">
-      <body className={inter.className}>{children}</body>
+    <html lang="fa">
+      <body className={vazirMatn.className}>
+        <AntdRegistry>
+          <ConfigProvider
+            theme={{
+              token: {
+                fontFamily: vazirMatn.style.fontFamily,
+                colorPrimary: "#3177FF",
+              },
+              components: {
+                Input: {
+                  paddingBlock: 6,
+                },
+              },
+            }}
+          >
+            {children}
+          </ConfigProvider>
+        </AntdRegistry>
+      </body>
     </html>
   );
 }
