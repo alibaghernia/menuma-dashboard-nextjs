a1d88b579c06f8b7da4f1a2e6f49df3a204dc67e
Abolfazl Akbarzadeh <abolfazlakbarzadeh80@gmail.com>
2024-02-06 14:08:42 +0330

    fix no-business's loading

--- src/app/(dashboard)/[business]/components/user/logout.tsx
+++ src/app/(dashboard)/[business]/components/user/logout.tsx
@@ -1,10 +1,19 @@
+"use client";
 import { Button } from "antd";
 import React from "react";
 import { logOut } from "./actions";
+import { useLoadings } from "@/utils/hooks";
+import { LOADINGS } from "@/providers/general/constants";
 
 const Logout = () => {
+  const [addL] = useLoadings();
   return (
-    <form action={logOut}>
+    <form
+      action={() => {
+        addL(LOADINGS.page);
+        logOut();
+      }}
+    >
       <Button htmlType="submit" block ghost danger type="primary">
         خروج
       </Button>
