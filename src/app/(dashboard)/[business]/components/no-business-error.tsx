0ab0b7b7e410c9a4191b2dcb8956161c0c5e675e
Abolfazl Akbarzadeh <abolfazlakbarzadeh80@gmail.com>
2024-02-08 14:26:08 +0330

    improve error handling

--- src/app/(dashboard)/[business]/components/no-business-error.tsx
+++ src/app/(dashboard)/[business]/components/no-business-error.tsx
@@ -3,10 +3,12 @@ import { Flex, Result } from "antd/lib";
 import React from "react";
 import Logout from "./user/logout";
 
-const NoBusinessError = () => {
+const NoBusinessError = ({
+  message = "شما مجوز لازم برای دسترسی به پنل را ندارید!",
+}) => {
   return (
     <Flex className="min-h-screen" align="center" justify="center">
-      <Result status="403" extra="شما مجوز لازم برای دسترسی به پنل را ندارید!">
+      <Result status="403" extra={message}>
         <Logout />
       </Result>
     </Flex>
--- src/app/(dashboard)/[business]/layout.tsx
+++ src/app/(dashboard)/[business]/layout.tsx
@@ -9,16 +9,30 @@ const DashboardLayout: FC<PropsWithChildren> = async ({
   children,
   params,
 }: any) => {
+  const business_uuid = params.business;
+  const business_uuid_isvalid =
+    /^[0-9a-f]{8}-[0-9a-f]{4}-[0-5][0-9a-f]{3}-[089ab][0-9a-f]{3}-[0-9a-f]{12}$/i.test(
+      business_uuid
+    );
   const session = await auth();
   const business = session?.user?.businesses?.find(
-    (buss) => buss.uuid == params.business
+    (buss) => buss.uuid == business_uuid
   );
 
   if (
+    !business_uuid_isvalid ||
     !business ||
     !["manager", "employee"].includes(business?.BusinessUser?.role)
   )
-    return <NoBusinessError />;
+    return (
+      <NoBusinessError
+        message={
+          business_uuid_isvalid
+            ? "شما مجوز لازم برای دسترسی به پنل را ندارید!"
+            : "شناسه کسب و کار نامعتبر است!"
+        }
+      />
+    );
   return (
     <BusinessProvider session={session!} business={business}>
       <PanelTemplate session={session!}>{children}</PanelTemplate>
